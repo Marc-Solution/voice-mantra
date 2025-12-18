42a72cfd1bda5cf74ce4479a7c7805138cd37033
Marco Deb <marco.deb@gmail.com>
2025-12-18 11:24:13 +0100

    Initial Commit

---
+++ VoiceMantra/VoiceMantraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMantraApp.swift
+//  VoiceMantra
+//
+//  Created by Marco Deb on 2025-12-18.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMantraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
